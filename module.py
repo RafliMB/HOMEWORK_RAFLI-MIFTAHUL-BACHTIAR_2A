64d1c7055572df25ef8fb4e69f731f60e4a2b617
Rafli Miftahul B <116947388+RafliMB@users.noreply.github.com>
2024-05-22 17:16:19 +0700

    module.py

--- module.py
+++ module.py
@@ -1,44 +1,40 @@
 stok = []
 
 def add():
-    input_nama = input("Masukkan nama barang : ").title()
+    input_nama = input("Masukkan nama barang : ").lower()
     input_stok = int(input("Masukkan stok barang : "))
     item = {'nama' : input_nama, 'stok' : input_stok}
     stok.append(item)
     print("--- Data berhasil ditambahkan ---")
-    return '\n'
 
 def edit():
     index = int(input("Ubah data ke : "))
     index -= 1
-    input_nama = input("Masukkan nama barang : ").title()
+    input_nama = input("Masukkan nama barang : ").lower()
     input_stok = int(input("Masukkan stok barang : "))
     data_change = {'nama' : input_nama, 'stok' : input_stok}
     stok[index] = data_change
     print("--- Data berhasil diperbarui ---")
-    return '\n'
 
 def delete():
     index = int(input("Hapus data ke : "))
     index -= 1
     del stok[index]
     print("--- Data berhasil dihapus ---")
-    return '\n'
 
 def search():
-    data_input = input("Cari nama barang : ").title()
+    data_input = input("Cari nama barang : ").lower()
     number = 1
-    stock = []
+    items = []
     for i in stok:
         if data_input in i['nama']:
             stock.append(i)
-    if stock:
-        for i in stock:
+    if items:
+        for i in items:
             print(f"{number}. {i['nama']} , Stok : {i['stok']}")
             number += 1
     else:
         print("--- Data barang tidak ditemukan ---")
-    return '\n'
 
 def list():
     number = 1
@@ -49,8 +45,6 @@ def list():
             number += 1
     else:
         print("--- Data barang kosong ---")
-    return '\n'
 
 def total():
     print(f"Jumlah data tersimpan : {len(stok)}")
-    return '\n'
